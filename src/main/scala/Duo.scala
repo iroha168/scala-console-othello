884a03324e81aee6392911d4a807c219eada0c84
iroha168 <cnoizecehiky@yahoo.co.jp>
2018-05-31 12:35:34 +0900

    finally it worksgit add src/*!

--- src/main/scala/Duo.scala
+++ src/main/scala/Duo.scala
@@ -1,5 +1,5 @@
-case class Duo(val player:Player, val opponent: Player, val board: Board) {
-  def move(coordinate: Coordinate, playerColoer: Cell,opponentrColoer: Cell): Board = {
+case class Duo(val board: Board, val player: Player, val opponent: Player, val blackPlayer: Player, val whitePlayer: Player) {
+  def move(coordinate: Coordinate, playerColoer: Cell, opponentrColoer: Cell): Board = {
     player.move(coordinate, playerColoer, opponentrColoer, board)
   }
 
@@ -7,5 +7,23 @@ case class Duo(val player:Player, val opponent: Player, val board: Board) {
     this.copy(player = opponent, opponent = player)
   }
 
+  def switch(target: Player): Duo = {
+    this.copy(blackPlayer = opponent, whitePlayer = player)
+  }
 
+  def playerColor: Cell = {
+    if (player == blackPlayer) Black
+    else White
+  }
+
+  def opponentColor: Cell = {
+    if (opponent == blackPlayer) Black
+    else White
+  }
+}
+
+object Duo {
+  def apply(board: Board, black: Player, white: Player): Duo = {
+    new Duo(board, black, white, black, white)
+  }
 }
